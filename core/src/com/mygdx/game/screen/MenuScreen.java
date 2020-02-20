f6a0b11fa93bf1c5a8a0987a471b321841ba7741
fsafia <fsafia@yandex.ru>
2020-02-20 18:51:44 +0300

    lesson2_ClassWork_Движение_картинки_за мышью

--- core/src/com/mygdx/game/screen/MenuScreen.java
+++ core/src/com/mygdx/game/screen/MenuScreen.java
@@ -7,40 +7,21 @@ import com.badlogic.gdx.math.Vector2;
 import com.mygdx.game.base.BaseScreen;
 
 public class MenuScreen extends BaseScreen {
-//    private Texture img;
-//
-//    @Override
-//    public void show() {
-//        super.show();
-//        img = new Texture("badlogic.jpg");
-//    }
-//
-//    @Override
-//    public void render(float delta) {
-//        super.render(delta);
-//        Gdx.gl.glClearColor(0.5f, 0.9f, 0.4f, 1);
-//        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-//        batch.begin();
-//        batch.draw(img, 0, 0);
-//        batch.end();
-//    }
-//
-//    @Override
-//    public void dispose() {
-//        img.dispose();
-//        super.dispose();
-//    }
-private Texture img;
+
+    private Texture img;
     private Vector2 touch;
     private Vector2 v;
     private Vector2 pos;
+    private Vector2 n;
+    private volatile float xCurrent;
+    private float yCurrent;
 
     @Override
     public void show() {
         super.show();
         img = new Texture("badlogic.jpg");
         touch = new Vector2();
-        v = new Vector2(1f,1f);
+        v = new Vector2();
         pos = new Vector2();
     }
 
@@ -49,9 +30,28 @@ private Texture img;
         super.render(delta);
         Gdx.gl.glClearColor(0.5f, 0.9f, 0.4f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        if ((pos.y + img.getHeight()) < Gdx.graphics.getHeight()) {
-            pos.add(v);
+
+        if (((pos.y + img.getHeight()) <= Gdx.graphics.getHeight()) && (pos.x + img.getWidth()) <= Gdx.graphics.getWidth() && pos.y >= 0 && pos.x >= 0) {
+
+            if (pos.x < xCurrent){
+                v = n;
+                pos.add(v.nor());
+                System.out.println("pos.x = " + pos.x + "pos.y = " + pos.y);
+                System.out.println(" xCurrent = " + xCurrent + "yCurrent " + yCurrent);
+
+            } else {
+                v.set(0f,0f);
+            }
+             if(pos.x > xCurrent) {
+                 v = n;
+                pos.add(v.nor());
+                System.out.println("tX = " + touch.x + "tY = " + touch.y);
+                System.out.println("pX = " + pos.x + "pY = " + pos.y);
+                System.out.println(" xCurrent = " + xCurrent + "yCurrent " + yCurrent);
+            }
+
         }
+
         batch.begin();
         batch.draw(img, pos.x, pos.y);
         batch.end();
@@ -68,6 +68,9 @@ private Texture img;
         super.touchDown(screenX, screenY, pointer, button);
         touch.set(screenX, Gdx.graphics.getHeight() - screenY);
         System.out.println("touch.x = " + touch.x + " touch.y " + touch.y);
+        xCurrent = touch.x;
+        yCurrent = touch.y;
+        n = touch.sub(pos);
         return false;
     }
 
